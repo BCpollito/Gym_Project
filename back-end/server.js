aed1c628b4f20463b696cbb51c518e21698e8567
Victor <victo.elcatire75@gmail.com>
2024-11-26 15:33:30 -0500

    intento de menejo de dos tipos diferentes de conexiones a base de datos

--- back-end/server.js
+++ back-end/server.js
@@ -14,6 +14,7 @@ app.use(
   })
 );
 
+let sequelize
 /* const sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
@@ -24,8 +25,20 @@ app.use(
             port: process.env.DB_PORT
         }
     ); */
-
-const sequelize = new Sequelize(process.env.MYSQL_PUBLIC_URL);
+    try {
+      const sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: process.env.DB_DIALECT,
+            port: process.env.DB_PORT
+        }
+    );
+    } catch (error) {
+      sequelize = new Sequelize(process.env.MYSQL_PUBLIC_URL);
+    }
 
 //modelo de la tabla registro de cliente
 const registro = sequelize.define(
