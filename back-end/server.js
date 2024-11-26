7037c46ff740ba8b422c27614c259aab5b71248b
Victor <victo.elcatire75@gmail.com>
2024-11-25 23:16:54 -0500

    cambios en la base de datos (railway)

--- back-end/server.js
+++ back-end/server.js
@@ -12,16 +12,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type'],  // Encabezados permitidos
 }));
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT
-    }
-);
+const sequelize = new Sequelize(process.env.MYSQL_PUBLIC_URL);
 
 //modelo de la tabla registro de cliente
 const registro = sequelize.define('registro', {
