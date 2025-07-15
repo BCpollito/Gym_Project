64f6194a47376b277bb5bae79047a6eca4305d8c
BCpollito <victo.elcatire75@gmail.com>
2025-07-15 14:50:42 -0500

    añadiendo tipado objeto cliente

---
+++ front/src/types/Cliente.ts
@@ -0,0 +1,11 @@
+export interface Cliente {
+  id: number;
+  name: string;
+  usuario: string;
+  password: string;
+  weight: string;
+  height: string;
+  age: string;
+  sex: string;
+  isAdmin: boolean;
+}
