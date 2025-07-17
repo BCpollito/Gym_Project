ec30cd51c0fc27c77f57d81b6a745bfad67ba3ad
BCpollito <victo.elcatire75@gmail.com>
2025-07-17 12:06:25 -0500

    feat: creating component workout (without functions)

---
+++ front/src/Components/libraryWorkout.tsx
@@ -0,0 +1,10 @@
+export default function LibraryWorkout(){
+    return (
+        <>
+        <div>
+            <h1>Página de Librería WORKOUT</h1>
+            <p>Esta es la página de la librería para la creación de workouts.</p>
+        </div>
+        </>        
+    );
+}
