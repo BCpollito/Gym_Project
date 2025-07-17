d44bf46ce12d36e73ce5f0f38e400321fee6bafd
BCpollito <victo.elcatire75@gmail.com>
2025-07-17 12:00:06 -0500

    feat: adding folder "navigation"

---
+++ front/src/navigation/libraryNavigation.tsx
@@ -0,0 +1,70 @@
+import { useNavigate } from "react-router-dom";
+import {
+  IconButton,
+  Collapse,
+  Chip,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+} from "@material-tailwind/react";
+import { Dumbbell, CalendarDays, List as ListIcon, X } from "lucide-react";
+import { useState } from "react";
+
+export default function LibraryNavigation() {
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = () => setOpen((cur) => !cur);
+  const navigate = useNavigate();
+
+  const handleLinkWorkout = () => {
+    navigate("/admin/libreria/activity/exercises");
+    setOpen(false);
+  };
+
+  const handleLinkPograms = () => {};
+
+  return (
+    <>
+      <div className="fixed top-0 left-0 w-full px-4 pt-2 bg-white shadow z-50">
+        <div className="flex items-center justify-between w-full">
+          <h1 className="text-lg font-semibold text-gray-800">Librería</h1>
+          <IconButton
+            onClick={toggleOpen}
+            className="mb-2"
+            variant="text"
+            size="lg"
+          >
+            {open ? (
+              <X className="h-6 w-6" />
+            ) : (
+              <ListIcon className="h-6 w-6" />
+            )}
+          </IconButton>
+        </div>
+        <Collapse open={open}>
+          <List>
+            <ListItem
+              onClick={handleLinkWorkout}
+              className="justify-center text-center text-gray-500"
+            >
+              <ListItemPrefix>
+                <Dumbbell className="h-4 w-4" />
+              </ListItemPrefix>
+              <Typography className="text-sm">Actividades</Typography>
+            </ListItem>
+            <ListItem
+              onClick={handleLinkPograms}
+              className="justify-center text-center text-gray-500"
+            >
+              <ListItemPrefix>
+                <CalendarDays className="h-4 w-4" />
+              </ListItemPrefix>
+              <Typography className="text-sm">Programas</Typography>
+            </ListItem>
+          </List>
+        </Collapse>
+      </div>
+    </>
+  );
+}
---
+++ front/src/navigation/mainNavegacion.tsx
@@ -0,0 +1,52 @@
+import { Button } from '@material-tailwind/react';
+import { Users, BookOpenText } from 'lucide-react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Navegacion() {
+  const [ clickClients, setClickClients ] = useState(true);
+  const [ clickLibrary, setClickLibrary ] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleClickClientes = () => {
+    setClickClients(true);
+    setClickLibrary(false);
+    navigate('/admin/clientes'); // Redirige a la página de clientes
+  };
+
+  const handleClickLibreria = () => {
+    setClickLibrary(true);
+    setClickClients(false);
+    navigate('/admin/libreria/activity/exercises'); // Redirige a la página de librería
+  };
+
+  return (
+    <div className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 z-50">
+      <div className="flex justify-around py-2">
+        {/* @ts-expect-error */}
+        <Button
+          onClick={handleClickClientes}
+          variant={clickClients ? 'filled' : 'text'}
+          size='sm'
+          color={clickClients ? 'blue' : 'gray'}
+          className="flex flex-col items-center p-1 min-w-0"
+        >
+          <Users size={16} />
+          <span className='text-[10px]'>Clientes</span>
+        </Button>
+        {/* @ts-expect-error */}
+        <Button
+          onClick={handleClickLibreria}
+          variant={clickLibrary ? 'filled' : 'text'}
+          size='sm'
+          color={clickLibrary ? 'blue' : 'black'}
+          className="flex flex-col items-center p-1 min-w-0"
+        >
+          <BookOpenText size={16} />
+          <span className='text-[10px]'>Libreria</span>
+        </Button>
+      </div>
+    </div>
+  );
+}
