f34ebdc0e955bee37ca8f1edf120d21ec30bd40f
BCpollito <victo.elcatire75@gmail.com>
2025-07-18 15:57:58 -0500

    feat: now, add exercises is possible

--- front/src/Components/addExerciseModal.tsx
+++ front/src/Components/addExerciseModal.tsx
@@ -11,15 +11,34 @@ import {
 import { PropsModal } from "../types/propsModal";
 import convertirLink from "../services/ConvertLink";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Exercise } from "../types/Exercises";
 
 export default function AddexerciseModal({ open, onClose }: PropsModal) {
   const [link, setlink] = useState<string | null>(null);
   const [convertedLink, setConvertedLink] = useState<string | null>(null);
+  const [ejercicio, setEjercicio] = useState<Exercise>({
+    ID_ejercicio: 0,
+    Nombre: "",
+    Descripcion: "",
+    Link: "",
+  });
 
   useEffect(() => {
     if (link) {
       const result = convertirLink(link);
       setConvertedLink(result);
+      if(result !== null){
+        setEjercicio({
+          ...ejercicio,
+          Link: result,
+        });
+      }else{
+        setEjercicio({
+          ...ejercicio,
+          Link: "",
+        });
+      }
     } else {
       setConvertedLink(null);
     }
@@ -29,18 +48,50 @@ export default function AddexerciseModal({ open, onClose }: PropsModal) {
     if (!open) {
       setlink(null);
     }
-  }, [open])
-  
+  }, [open]);
+
+  const handleCreate = async () => {
+    try {
+      const response = await axios.post<{
+        success: boolean;
+        message?: string;
+      }>('/ejercicio', {
+        Nombre: ejercicio.Nombre,
+      Descripcion: ejercicio.Descripcion,
+      Link: ejercicio.Link,
+      });
+
+      if(response.data.success) {
+        setEjercicio({
+          ...ejercicio,
+          Nombre: "",
+          Descripcion: "",
+          Link: "",
+        });
+        onClose();
+      }
+
+      alert(`${response.data.message}`)      
+
+    } catch (error) {
+      console.error("Error al crear el ejercicio:", error);
+    }    
+  };
 
   return (
     <>
-      <Dialog size="sm" open={open} handler={onClose} className="p-4">
+      <Dialog
+        size="sm"
+        open={open}
+        handler={onClose}
+        className="p-4 max-h-[80vh]"
+      >
         <DialogHeader className="relative m-0 block">
           <Typography variant="h4" color="blue-gray">
             Añadir Ejercicio
           </Typography>
         </DialogHeader>
-        <DialogBody className="space-y-4 pb-6 overflow-scroll">
+        <DialogBody className="space-y-4 pb-6 overflow-y-auto max-h-[60vh]">
           <div>
             <Typography
               variant="small"
@@ -61,6 +112,12 @@ export default function AddexerciseModal({ open, onClose }: PropsModal) {
               labelProps={{
                 className: "hidden",
               }}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setEjercicio({
+                  ...ejercicio,
+                  Nombre: e.target.value,
+                })
+              }
             />
           </div>
           <div>
@@ -71,8 +128,7 @@ export default function AddexerciseModal({ open, onClose }: PropsModal) {
             >
               Link (imagen o video)
             </Typography>
-            <Input
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setlink(e.target.value)}
+            <Input              
               color="gray"
               size="lg"
               placeholder="https://www.youtube.com/watch?v=example"
@@ -84,6 +140,9 @@ export default function AddexerciseModal({ open, onClose }: PropsModal) {
               labelProps={{
                 className: "hidden",
               }}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setlink(e.target.value)
+              }
             />
           </div>
           <div>
@@ -101,26 +160,46 @@ export default function AddexerciseModal({ open, onClose }: PropsModal) {
               labelProps={{
                 className: "hidden",
               }}
+              onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                setEjercicio({
+                  ...ejercicio,
+                  Descripcion: e.target.value,
+                })
+              }
             />
           </div>
-          <div>
-    {convertedLink === null && link ? (
-      <Typography color="red" variant="h6">URL no válida</Typography>
-    ) : convertedLink && convertedLink.includes("youtube.com/embed") ? (
-      <iframe
-        className="max-w-sm aspect-video"
-        src={convertedLink}
-        title="YouTube video"
-        allowFullScreen
-        controls
-      />
-    ) : convertedLink ? (
-      <img className="max-w-sm" src={convertedLink} alt="Imagen de ejercicio" />
-    ) : null}
-  </div>
+          <div className="flex flex-col items-center justify-center">
+            {convertedLink === null && link ? (
+              <Typography color="red" variant="h6">
+                URL no válida
+              </Typography>
+            ) : convertedLink && convertedLink.includes("youtube.com/embed") ? (
+              <iframe
+                className="max-w-sm aspect-video"
+                src={convertedLink}
+                title="YouTube video"
+                allowFullScreen
+              />
+            ) : convertedLink ? (
+              <img
+                className="max-w-sm"
+                src={convertedLink}
+                alt="Imagen no encontrada"
+              />
+            ) : null}
+          </div>
         </DialogBody>
-        <DialogFooter>
-          <Button className="ml-auto">Add Product</Button>
+        <DialogFooter className="flex ">
+          <Button
+            variant="outlined"
+            className="hover:bg-red-700 hover:text-white"
+            onClick={onClose}
+          >
+            Cancelar
+          </Button>
+          <Button onClick={handleCreate} className="ml-auto">
+            Crear
+          </Button>
         </DialogFooter>
       </Dialog>
     </>
