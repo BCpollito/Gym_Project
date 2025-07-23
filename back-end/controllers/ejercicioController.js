4406a3f49e374941ab6829782554db102114b16f
BCpollito <victo.elcatire75@gmail.com>
2025-07-23 16:07:36 -0500

    feat: improving exercisecontroller

--- back-end/controllers/ejercicioController.js
+++ back-end/controllers/ejercicioController.js
@@ -5,16 +5,16 @@ exports.createEjercicio = async (req, res) => {
   try {
     const { Nombre, Descripcion, Link, Tag } = req.body;
 
-    if (!Nombre.trim() || !Link.trim()) {
-      return res.json({success: false, message: "Nombre y Link son requeridos" });
-    }else{
-       const nuevoEjercicio = await Ejercicio.create({
-      Nombre,
-      Descripcion,
-      Link,
-      Tag,
-    });
-    return res.status(201).json({ nuevoEjercicio: nuevoEjercicio, message: "Ejercicio creado exitosamente" , success: true });
+    if (!Nombre.trim() || !Link.trim() || !Tag.trim()) {
+      return res.json({ success: false, message: "Algunos Campos son obligatorios" });
+    } else {
+      const nuevoEjercicio = await Ejercicio.create({
+        Nombre,
+        Descripcion,
+        Link,
+        Tag,
+      });
+      return res.status(201).json({ nuevoEjercicio: nuevoEjercicio, message: "Ejercicio creado exitosamente", success: true });
     }
   } catch (error) {
     return res.status(500).json({ error: error.message });
@@ -35,14 +35,20 @@ exports.getEjercicios = async (req, res) => {
 exports.updateEjercicio = async (req, res) => {
   try {
     const { id } = req.params;
-    const { Nombre, Descripcion, Link } = req.body;
-    await Ejercicio.update(
-      { Nombre, Descripcion, Link, Tag},
-      { where: { ID_ejercicio: id } }
-    );
-    res.json({ message: "Ejercicio actualizado" });
+    const { Nombre, Descripcion, Link, Tag } = req.body;
+
+    if (!Nombre.trim() || !Link.trim() || !Tag.trim()) {
+      return res.json({ success: false, message: "Algunos Campos son obligatorios" });
+    } else {
+      await Ejercicio.update(
+        { Nombre, Descripcion, Link, Tag },
+        { where: { ID_ejercicio: id } }
+      );
+    }
+
+    return res.json({ message: "Ejercicio actualizado", success: true });
   } catch (error) {
-    res.status(500).json({ error: "Error al actualizar el ejercicio" });
+    res.status(500).json({ error: error });
   }
 };
 
