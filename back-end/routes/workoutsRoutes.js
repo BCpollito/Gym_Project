a675d3054d554b7d64930f49db15f89a51de87fa
BCpollito <victo.elcatire75@gmail.com>
2025-07-28 14:03:51 -0500

    feat: workouts routes

---
+++ back-end/routes/workoutsRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const Workouts = require("../controllers/workoutsController");
+
+router.post('/workouts', Workouts.createWorkout);
+router.get('/workouts', Workouts.getAllWorkout);
+router.put('/workouts/:id', Workouts.updateWorkout);
+router.delete('/workouts/:id', Workouts.deleteWorkout);
