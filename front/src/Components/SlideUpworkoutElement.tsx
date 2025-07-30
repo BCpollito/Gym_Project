dd9db23a4fe486f0fbe75765edb610a3974b3b29
BCpollito <victo.elcatire75@gmail.com>
2025-07-30 17:25:02 -0500

    feat: add bloque to workout and workoutelement added

--- front/src/Components/SlideUpworkoutElement.tsx
+++ front/src/Components/SlideUpworkoutElement.tsx
@@ -2,12 +2,19 @@ import {
   Dialog,
   DialogHeader,
   DialogBody,
+  DialogFooter,
   Typography,
   IconButton,
+  Input,
+  Textarea,
+  Button,
 } from "@material-tailwind/react";
 import { X, LayoutList, CirclePause } from "lucide-react";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { PropsModal } from "../types/propsModal";
+import axios from "axios";
+import { Workout } from "../types/workout";
+import { WorkoutElement } from "../types/WorkoutElement";
 
 export default function SlideUpworkoutElement({
   open,
@@ -15,9 +22,54 @@ export default function SlideUpworkoutElement({
   idworkout,
   modo,
 }: PropsModal) {
-  useEffect(() => {
-    console.log(idworkout);
-  }, []);
+  const [nombre, setnombre] = useState<string>("");
+  const [descripcion, setdescripcion] = useState<string>("");
+  const [orden, setorden] = useState<number>(1);
+
+  const handleAddElement = async () => {
+    if (modo === "Bloque") {
+      console.log(idworkout);
+      console.log(Number(idworkout));
+      const response = await axios.post<{
+        success: boolean;
+        message: string;
+        newblock: Workout;
+      }>("/bloques", {
+        workoutID: idworkout,
+        nombre: nombre,
+        descripcion: descripcion,
+      });
+
+      const responseElement = await axios.post<{
+        success: boolean;
+        message: string;
+        newElement: WorkoutElement;
+      }>("/workoutElement", {     
+        workoutID: idworkout,   
+        tipo: modo,
+        elementoID: response.data.newblock.id,
+        orden: orden,
+      });
+
+      if (response.data.success) {
+        if (responseElement.data.success) {
+          const newOrden = orden + 1;
+          setorden(newOrden);
+          console.log(
+            `Se agrego un nuevo elemento tipo: ${responseElement.data.newElement.tipo} al workoutde ID: ${responseElement.data.newElement.workoutID}`
+          );
+        }
+        alert(`${response.data.message}`);
+        setnombre("");
+        setdescripcion("");
+        onClose();
+      }
+    }
+
+    if (modo === "Descanso") {
+      console.log("DESCANSO");
+    }
+  };
 
   return (
     <>
@@ -32,29 +84,89 @@ export default function SlideUpworkoutElement({
         style={{ maxHeight: "80vh", height: "auto" }}
       >
         <DialogHeader className="flex justify-between items-center w-full">
-          <div className="w-10" />
+          <div className="w-6" />
           <div className="flex gap-1">
-             {modo === "Bloque" && <LayoutList />}
-              {modo === "Descanso" && <CirclePause/>}
+            {modo === "Bloque" && <LayoutList />}
+            {modo === "Descanso" && <CirclePause />}
             <Typography variant="h6" className="text-center flex-1">
               {modo === "Bloque" && "Añadir Bloque"}
               {modo === "Descanso" && "Añadir Descanso"}
             </Typography>
           </div>
           <IconButton
-            size="sm"
-            color="white"
-            variant="filled"
-            className="shadow-sm shadow-secondary rounded-full"
+            variant="text"
+            className=" rounded-full"
             onClick={onClose}
           >
             <X />
           </IconButton>
         </DialogHeader>
+        {modo === "Bloque" && (
+          <DialogBody className="w-full p-5 pt-0 space-y-4 pb-6">
+            <Typography color="black" variant="h6">
+              Informacion del bloque
+            </Typography>
+            <div>
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="mb-2 text-left font-medium"
+              >
+                Nombre
+              </Typography>
+              <Input
+                color="gray"
+                size="lg"
+                placeholder="ej. Fuerza"
+                value={nombre}
+                className="placeholder:opacity-100 focus:!border-t-gray-900"
+                containerProps={{
+                  className: "!min-w-full",
+                }}
+                labelProps={{
+                  className: "hidden",
+                }}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setnombre(e.target.value)
+                }
+              />
+            </div>
 
-        <DialogBody className="w-full p-0 pb-4">
-          <Typography variant="h4">Contenido...</Typography>
-        </DialogBody>
+            <div>
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="text-left font-medium flex"
+              >
+                Descripcion
+                <Typography color="gray" variant="small">
+                  (Opcional)
+                </Typography>
+              </Typography>
+              <Textarea
+                rows={5}
+                placeholder="ej. ejercicios Leves..."
+                value={descripcion}
+                className="!w-full !border-[1.5px] !border-blue-gray-200/90 !border-t-blue-gray-200/90 bg-white text-gray-600 ring-4 ring-transparent focus:!border-primary focus:!border-t-blue-gray-900 group-hover:!border-primary"
+                labelProps={{
+                  className: "hidden",
+                }}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                  setdescripcion(e.target.value)
+                }
+              />
+            </div>
+          </DialogBody>
+        )}
+        <DialogFooter className="flex items-center justify-center">
+          <Button
+            onClick={handleAddElement}
+            color="orange"
+            className="rounded-full w-full "
+          >
+            Continuar
+          </Button>
+        </DialogFooter>
       </Dialog>
     </>
   );
