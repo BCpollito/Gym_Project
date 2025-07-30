2eea92824602a12ff945359eabac12d296393082
BCpollito <victo.elcatire75@gmail.com>
2025-07-30 17:20:45 -0500

    feat: bloques controller created

---
+++ back-end/controllers/bloquesController.js
@@ -0,0 +1,52 @@
+const { Bloques } = require("../models");
+
+//crear bloque
+exports.createblock = async (req, res) => {
+    try {
+        const { workoutID, nombre, descripcion } = req.body;
+
+        if (!nombre.trim()) {
+            return res.json({ success: false, message: "El nombre es obligatorio" });
+        } else {
+            const newblock = await Bloques.create({
+                workoutID,
+                nombre,
+                descripcion
+            });
+            return res.status(201).json({ newblock: newblock, message: "bloque creado exitosamente", success: true });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+//actualizar workout
+exports.updateBlock = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nombre, descripcion } = req.body;
+
+        if (!nombre.trim()) {
+            return res.json({ success: false, message: "El nombre es obligatorio" });
+        } else {
+            await Bloques.update(
+                { nombre, descripcion },
+                { where: { id: id } }
+            );
+            return res.status(201).json({ message: "Bloque actualizado exitosamente", success: true });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+};
+
+exports.deleteWorkout = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Bloques.destroy({ where: { id: id } });
+        return res.json({message: "Bloque eliminado satisfactoriamente"})
+    } catch (error) {
+        return res.json({error: `Algo salio mal: ${error}`});
+    }
+};
