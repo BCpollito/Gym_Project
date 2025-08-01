fb2c4ca9bc1cd313554a4eed1b0aec4c1f1f25fd
BCpollito <victo.elcatire75@gmail.com>
2025-08-01 16:20:13 -0500

    feat: descansosController created

--- back-end/controllers/descansosController.js
+++ back-end/controllers/descansosController.js
@@ -1,50 +1,30 @@
-/* const { Descansos } = require("../models");
+const { Descansos } = require("../models");
 
-//crear bloque
-exports.createblock = async (req, res) => {
+exports.createDescanso = async (req, res) => {
     try {
-        const { nombre, descripcion } = req.body;
-
-        if (!nombre.trim()) {
-            return res.json({ success: false, message: "El nombre es obligatorio" });
-        } else {
-            const newblock = await Bloques.create({
-                nombre,
-                descripcion
-            });
-            return res.status(201).json({ newblock: newblock, message: "bloque creado exitosamente", success: true });
-        }
+        const { workoutID, duracionSegundos} = req.body;
+        const newRest = await Descansos.create({
+            workoutID,
+            duracionSegundos
+        });
+        return res.status(201).json({ newRest: newRest, message: "descanso creado exitosamente", success: true });
     } catch (error) {
-        return res.status(500).json({ error: error.message });
+        console.log(error);
+        return res.status(500).json({ message: error.message, success: false });
     }
-}
 
-//actualizar workout
-exports.updateBlock = async (req, res) => {
-    try {
+};
+
+exports.updateDescanso = async (req, res) => {
+    try{
         const { id } = req.params;
-        const { nombre, descripcion } = req.body;
+        const { duracionSegundos } = req.body;
 
-        if (!nombre.trim()) {
-            return res.json({ success: false, message: "El nombre es obligatorio" });
-        } else {
-            await Bloques.update(
-                { nombre, descripcion },
-                { where: { id: id } }
-            );
-            return res.status(201).json({ message: "Bloque actualizado exitosamente", success: true });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error });
-    }
-}
+        await Descansos.update(
+            {duracionSegundos}, {where: {id: id}}
+        );
+        return res.status(201).json({ message: "descanso actualizado exitosamente", success: true });
+    }catch (error){
 
-exports.deleteWorkout = async (req, res) => {
-    try {
-        const { id } = req.params;
-        await Bloques.destroy({ where: { id: id } });
-        return res.json({ message: "Bloque eliminado satisfactoriamente" })
-    } catch (error) {
-        return res.json({ error: `Algo salio mal: ${error}` });
     }
-}; */
+};
