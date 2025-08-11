8994e8e913a74001878e00e56121805a799ca0a8
BCpollito <victo.elcatire75@gmail.com>
2025-08-11 17:52:27 -0500

    feat: adding reusable Rest times

---
+++ front/src/types/RestTimes.ts
@@ -0,0 +1,24 @@
+const chips = [
+  { label: "0s", value: 0 },
+  { label: "10s", value: 10 },
+  { label: "15s", value: 15 },
+  { label: "20s", value: 20 },
+  { label: "25s", value: 25 },
+  { label: "30s", value: 30 },
+  { label: "35s", value: 35 },
+  { label: "40s", value: 40 },
+  { label: "45s", value: 45 },
+  { label: "50s", value: 50 },
+  { label: "55s", value: 55 },
+  { label: "60s", value: 60 },
+  { label: "90s", value: 90 },
+  { label: "2m", value: 120 },
+  { label: "2.5m", value: 150 },
+  { label: "3m", value: 180 },
+  { label: "3.5m", value: 210 },
+  { label: "4m", value: 240 },
+  { label: "4.5m", value: 270 },
+  { label: "5m", value: 300 }
+];
+
+export { chips };
