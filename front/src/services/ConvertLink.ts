f89598c606f0cb5e066e47a3c4ecd4a68aa2afeb
BCpollito <victo.elcatire75@gmail.com>
2025-08-13 09:33:20 -0500

    feat: adding img converter for youtube videos

--- front/src/services/ConvertLink.ts
+++ front/src/services/ConvertLink.ts
@@ -3,12 +3,19 @@ export default function convertirLink(link: string): string | null {
   const regExpImg = /\.(jpg|jpeg|png|webp|gif|bmp|svg)(\?.*)?$/i;
 
   const match = link.match(regExp);
-  
+
+  if (link.includes("youtube.com/embed")) {
+    const match = link.match(/embed\/([a-zA-Z0-9_-]+)/);
+    const videoId = match ? match[1] : null;
+    if (!videoId) return null;
+    return `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+  }
+
   if (match && match[1]) {
     return `https://www.youtube.com/embed/${match[1]}`;
   }
 
-  
+
   if (regExpImg.test(link)) {
     return link;
   }
