f81e3f2292a3ec3f4c554a959c08af759153a251
BCpollito <victo.elcatire75@gmail.com>
2025-08-29 15:44:00 -0500

    feat: Rendering Workouts view

--- front/src/Components/libraryWorkout.tsx
+++ front/src/Components/libraryWorkout.tsx
@@ -6,27 +6,18 @@ import {
   Button,
 } from "@material-tailwind/react";
 import { Plus } from "lucide-react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import CreateWorkoutModal from "./CreateWorkoutModal";
+import ScrollToTopButton from "./ScrollToTopButton";
+import { Workout } from "../types/workout";
+import axios from "axios";
 
 const TABLE_HEAD = [
   {
-    head: "Number",
+    head: "Nombre",
   },
   {
-    head: "Customer",
-  },
-  {
-    head: "Amount",
-  },
-  {
-    head: "Issued",
-  },
-  {
-    head: "Payment Date",
-  },
-  {
-    head: "opciones prolongadas",
+    head: "Descripción",
   },
 ];
 
@@ -34,130 +25,40 @@ const TABLE_ROWS = [
   {
     number: "#MS-415646",
     customer: "Viking Burrito",
-    amount: "$14,000",
-    issued: "31 Jan 2024",
-    date: "31 Feb 2024",
   },
   {
     number: "#RV-126749",
     customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
   },
   {
     number: "#QW-103578",
     customer: "Fiber Notion",
-    amount: "$20,000",
-    issued: "12 Jan 2024",
-    date: "12 Feb 2024",
   },
   {
     number: "#MS-415688",
     customer: "Blue Bird",
-    amount: "$5,600",
-    issued: "10 Jan 2024",
-    date: "10 Feb 2024",
   },
   ,
   {
     number: "#RV-126749",
     customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
   },
   {
     number: "#RV-126749",
     customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
   },
   {
     number: "#RV-126749",
     customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
   },
 
   {
     number: "#RV-126749",
     customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
   },
   {
     number: "#RV-126749",
     customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
-  },
-  {
-    number: "#RV-126749",
-    customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
-  },
-  {
-    number: "#RV-126749",
-    customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
-  },
-  {
-    number: "#RV-126749",
-    customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
-  },
-  {
-    number: "#RV-126749",
-    customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
-  },
-  {
-    number: "#RV-126749",
-    customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
-  },
-  {
-    number: "#RV-126749",
-    customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
-  },
-  {
-    number: "#RV-126749",
-    customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
-  },
-  {
-    number: "#RV-126749",
-    customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
-  },
-  {
-    number: "#RV-126749",
-    customer: "Stone Tech Zone",
-    amount: "$3,000",
-    issued: "24 Jan 2024",
-    date: "24 Feb 2024",
   },
 ];
 
@@ -171,6 +72,23 @@ export default function LibraryWorkout() {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const scrollRef = useRef<HTMLDivElement>(null)
+
+  const [workouts, setWorkouts] = useState<Workout[]>([]);
+
+  useEffect(() => {
+    const getWorkouts = async () => {
+      try {
+        const response = await axios.get<Workout[]>('/workouts');
+        const WorkoutsData = response.data.sort((a, b) => b.id - a.id);
+        setWorkouts(WorkoutsData);
+      } catch (error) {
+        console.error("Error al cargar Workouts: ", error)
+      }
+    }
+    getWorkouts();
+  }, [])
 
   return (
     <div className="max-h-[77vh]">
@@ -188,18 +106,20 @@ export default function LibraryWorkout() {
         <Input label="Buscar Workout por nombre" />
       </div>
       {/*// @ts-ignore*/}
-      <Card className="h-full w-full sm:max-w-sm overflow-scroll ">
+      <Card ref={scrollRef} className="h-full w-full sm:max-w-sm overflow-scroll">
         {/*// @ts-ignore*/}
-        <CardBody className="max-w-sm max-h-[72vh] h-full">
-          <table className="overflow-scroll table-auto max-w-sm text-left">
+        <CardBody className="max-w-sm max-h-[72vh] h-full py-2">
+          <table className="w-full table-auto max-w-sm text-left">
             <thead>
               <tr>
+                <th></th>
                 {TABLE_HEAD.map(({ head }) => (
                   <th
                     key={head}
                     className="border-b border-gray-300 text-center"
                   >
-                    <div className="flex items-center gap-1">
+                    <div className="flex justify-center items-center gap-1">
+                      {/*// @ts-ignore*/}
                       <Typography
                         color="blue-gray"
                         variant="small"
@@ -213,67 +133,51 @@ export default function LibraryWorkout() {
               </tr>
             </thead>
             <tbody>
-              {TABLE_ROWS.map(
-                ({ number, customer, amount, issued, date }, index) => {
-                  const isLast = index === TABLE_ROWS.length - 1;
+              {workouts?.map((workout, index) => {
+                const isLast = index === workouts.length - 1;
                   const classes = isLast
                     ? "p-4"
                     : "p-4 border-b border-gray-300";
 
-                  return (
-                    <tr key={number}>
-                      <td className={classes}>
-                        <div className="flex items-center gap-1">
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-bold"
-                          >
-                            {number}
-                          </Typography>
-                        </div>
-                      </td>
-                      <td className={classes}>
+                return (
+                  <tr key={workout.id}>
+                    <td>
+                      <Button></Button>
+                    </td>
+                    <td className={classes}>
+                      <div className="flex items-center gap-1">
+                        {/*// @ts-ignore*/}
                         <Typography
                           variant="small"
-                          className="font-normal text-gray-600"
+                          color="blue-gray"
+                          className="font-bold"
                         >
-                          {customer}
+                          {workout.nombre}
                         </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          className="font-normal text-gray-600"
-                        >
-                          {amount}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          className="font-normal text-gray-600"
-                        >
-                          {issued}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          className="font-normal text-gray-600"
-                        >
-                          {date}
-                        </Typography>
-                      </td>
-                    </tr>
-                  );
-                }
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      {/*// @ts-ignore*/}
+                      <Typography
+                        variant="small"
+                        className="font-normal text-gray-600"
+                      >
+                        {workout.descripcion || "sin descripcion"}
+                      </Typography>
+                    </td>
+                  </tr>
+                )
+
+              }
               )}
             </tbody>
           </table>
         </CardBody>
       </Card>
       <CreateWorkoutModal open={open} onClose={handleClose} />
+      <div className="fixed bottom-[70px] right-4 z-50">
+        <ScrollToTopButton scrollRef={scrollRef} />
+      </div>
     </div>
   );
 }
