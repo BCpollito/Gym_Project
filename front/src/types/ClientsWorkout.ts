664d4e5d1829c63ab235da640b2008669890b577
BCpollito <victo.elcatire75@gmail.com>
2025-09-18 15:33:56 -0500

    feat: adding type to clientworkout

--- front/src/types/ClientsWorkout.ts
+++ front/src/types/ClientsWorkout.ts
@@ -1,6 +1,9 @@
+import { Workout } from "./Workout/workout";
+
 export interface ClientsWorkout {
   id: number; 
   clienteID: number; 
   workoutID: number; 
   dateAssign: string;
+  Workout: Workout;
 }
