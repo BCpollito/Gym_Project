bff5881e0c25bb2716128471df3aac1b63ac8841
BCpollito <victo.elcatire75@gmail.com>
2025-10-02 09:52:03 -0500

    feat: new types added to calendaryprops

--- front/src/types/Calendary/CalendaryProps.ts
+++ front/src/types/Calendary/CalendaryProps.ts
@@ -1,4 +1,10 @@
+import { ClientsWorkout } from "../ClientsWorkout";
+
 export interface CalendaryProps {
 	mode?: "cliente";
 	idClient?: number;
+	signBack?: () => void;
+	setday?: (date :{day: number, month: string, year: Number}) => void;
+	setWorkoutsDay?: (ClientsWorkout: ClientsWorkout[]) => void;
+	refresh?: boolean;
 }
