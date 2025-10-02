251cf570e91eab265e0a8dd22e17428f4123a182
BCpollito <victo.elcatire75@gmail.com>
2025-10-02 09:15:52 -0500

    feat: rendering workouts in DayWorkoutModal and function to add new element added

--- front/src/Components/Calendary/DayWorkoutModal.tsx
+++ front/src/Components/Calendary/DayWorkoutModal.tsx
@@ -7,51 +7,57 @@ import {
 	Button,
 	List,
 	ListItem,
-	Card
+	IconButton
 } from "@material-tailwind/react";
-import { ClientsWorkout } from "../../types/ClientsWorkout";
+import {
+	Zap,
+	X
+} from "lucide-react";
+import { WorkoutClientData } from "../../types/Calendary/WorkoutClientData";
+import { mesesDelAño } from "../../types/Calendary/MonthsOfYears";
 
-interface wokoutClientData {
-	open: boolean;
-	onClose: () => void;
-	day: number | null;
-	workoutsDay?: ClientsWorkout[];
-}
-
-export default function DayWorkoutModal(wokoutClientData: wokoutClientData) {
+export default function DayWorkoutModal({ open, onClose, day, workoutsDay, signback }: WorkoutClientData) {
+	const handleAddNew = () => {
+		signback!();
+		onClose();
+	}
 	return (
 		<>
 			{/*@ts-ignore*/}
 			<Dialog color="red" open={open}
-				handler={wokoutClientData.onClose}
-				className="max-h-[80svh]"
+				handler={onClose}
+				className="h-[90svh]"
 			>
 				{/*@ts-ignore*/}
-				<DialogHeader>
+				<DialogHeader className="flex justify-between pb-0">
+					<div>
+						{/*@ts-ignore*/}
+						<Typography variant="h5">{`${day.day} ${mesesDelAño[day.month]} ${day.year}`}</Typography>
+						{/*@ts-ignore*/}
+						<Typography>Tareas del dia</Typography>
+					</div>
 					{/*@ts-ignore*/}
-					<Typography variant="h3">{`Programacion dia ${wokoutClientData.day}`}</Typography>
+					<IconButton className="mb-5" onClick={onClose} variant="text" size="sm"><X /></IconButton>
 				</DialogHeader>
 				{/*@ts-ignore*/}
-				<DialogBody className="overflow-y-scroll max-h-[50svh]">
+				<DialogBody className="overflow-y-scroll h-[68svh] pt-0">
 					{/*@ts-ignore*/}
-					<Card className="w-full">
-						{/*@ts-ignore*/}
-						<List color="red" className="w-full">
-							{wokoutClientData.workoutsDay?.map((w) => (
-								//@ts-ignore
-								<ListItem className="bg-green-200">
-									{/*@ts-ignore*/}
-									<Typography color="blue-gray" className="font-bold line-clamp-1">{w.Workout.nombre}</Typography>
-
-								</ListItem>
-							))}
-						</List>
-					</Card>
+					{/*@ts-ignore*/}
+					<List color="red" className="w-full">
+						{workoutsDay?.map((w) => (
+							//@ts-ignore
+							<ListItem className="border border-gray-500 p-2 grid grid-cols-1 rounded rounded-1">
+								{/*@ts-ignore*/}
+								<Typography variant="paragraph" className="font-bold line-clamp-1">{w.Workout.nombre}</Typography>
+								<span className="flex text-xs items-center gap-1"><Zap size={15} />Workout</span>
+							</ListItem>
+						))}
+					</List>
 				</DialogBody>
 				{/*@ts-ignore*/}
 				<DialogFooter>
 					{/*@ts-ignore*/}
-					<Button variant="outlined" color="red" onClick={wokoutClientData.onClose}>Cerrar</Button>
+					<Button onClick={handleAddNew} variant="gradient" color="amber">Añadir nuevo</Button>
 				</DialogFooter>
 			</Dialog>
 		</>
